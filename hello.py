47780d1fbef0a8c4b7ca13070bdad0c0d6fc5e2e
EmilyTain <yyyemily318@gmail.com>
2023-02-07 16:33:35 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+#Emily Anak Tain
+#20DDT21F1903
+#fahrenheit = (celcius * 1.8) + 32
+celcius = int (input("Please enter the tempature value in calcius?\n")) 
+f =  celcius * (9/5)+ 32
+print (f)
+
+
+
+
+
